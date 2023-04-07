532a4f48cf5c2d637856349b3ed65c062bdd9810
Thiago Souza <thiagolsmail@gmail.com>
2023-04-07 18:43:55 +0000

    criacao da view vw_sales no modelo do projeto

---
+++ target/run/analytics_dbt/models/views/vw_sales.sql
@@ -0,0 +1,26 @@
+
+
+  create view "dev"."public"."vw_sales__dbt_tmp" as (
+    WITH source_sales AS(
+    SELECT *
+    FROM sales
+),
+
+renamed AS (
+    SELECT salesid AS id_venda,
+           listid AS id_lista,
+           sellerid AS id_vendedor,
+           buyerid AS id_comprador,
+           eventid AS id_evento,
+           dateid AS id_date,
+           qtysold AS quantidade_vendida,
+           pricepaid AS valor_pago,
+           commission AS comissao,
+           to_char(saletime, 'dd/mm/yyyy HH24:MI:SS') AS data_hora_venda,
+           cast(saletime AS date) AS data_venda,
+           to_char(saletime, 'HH24:MI:SS') AS hora_venda
+    FROM source_sales
+)
+
+SELECT * FROM renamed
+  ) ;
